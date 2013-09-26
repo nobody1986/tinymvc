79be07dc44c057ad0e8de27f8caa53f83d6d499c
YE <NOBODY1986@163.COM>
2013-09-27 00:20:47 +0800

    bootstrap
    bootstrap

--- app/controller/friends.php
+++ app/controller/friends.php
@@ -1,6 +1,7 @@
 <?php
 class Friends_Controller extends Controller{
     function index(){
+        $_SERVER['SERVER_ADDR'] = '127.0.0.1';
         Core::import('lib.weibo.qq');
         $q = new Weibo_Qq('http://127.0.0.1:8080/friends/index');
 
--- app/index.php
+++ app/index.php
@@ -1,4 +1,5 @@
 <?php
 set_time_limit(200);
+session_start();
 require_once('config/config.php');
 Core::go();
